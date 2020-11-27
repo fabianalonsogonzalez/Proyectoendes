5e0b733ee8f7580d3d32a0de5d0b88042d536e70
fabian alonso <fabianag05@educastur.es>
2020-11-27 20:38:44 +0100

    initial comit

---
+++ PruebaEndes/src/endes/pruebas/main/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package endes.pruebas.main;
+
+/**
+ * @author: Tardes Alonso
+ * Fecha: 26 oct. 2020
+ * version: 1.0
+ */
+public class Main {
+
+	/**
+	 * Fecha Mod: 26 oct. 2020
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println();
+
+	}
+
+}
